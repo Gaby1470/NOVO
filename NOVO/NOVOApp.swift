978a94c182bdcdeaa5b89e367b34bfc3830397a5
Gaby1470 <gabriela.sanchez.alcaraz@gmail.com>
2024-02-18 14:48:32 -0800

    Initial Commit

---
+++ NOVO/NOVOApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOVOApp.swift
+//  NOVO
+//
+//  Created by Gabriela Sanchez on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NOVOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
